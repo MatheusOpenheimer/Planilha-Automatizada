a1a233107e825116faed18455c3dcc6c01aa14e3
Matheus Openheimer <matheushtml3000@gmail.com>
2026-06-02 07:24:41 -0300

    arrumando caminho_recurso

--- main.py
+++ main.py
@@ -63,7 +63,9 @@ def executar():
     excel.Quit()
 
 def caminho_recurso(relativo):
-    return os.path.join(os.path.abspath("."), relativo)     
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relativo)
+    return os.path.join(os.path.abspath("."), relativo)      
 #Janela
 janela = tk.Tk()
 janela.title("Planilha automatizada")
